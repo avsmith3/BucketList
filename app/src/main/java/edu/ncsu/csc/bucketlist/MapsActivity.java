3a4758b29f46d49ea3bd9528b4969b0f7e5ba08d
nnayak2 <nnayak2@ncsu.edu>
2015-05-02 10:41:40 -0400

    Half Working prototype

--- app/src/main/java/edu/ncsu/csc/bucketlist/MapsActivity.java
+++ app/src/main/java/edu/ncsu/csc/bucketlist/MapsActivity.java
@@ -1,5 +1,6 @@
 package edu.ncsu.csc.bucketlist;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.location.Address;
@@ -13,6 +14,8 @@ import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -37,6 +40,18 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
+import android.widget.AutoCompleteTextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -45,9 +60,15 @@ import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements LocationListener,
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback, GoogleMap.OnMyLocationChangeListener {
+        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback, GoogleMap.OnMyLocationChangeListener, OnItemClickListener {
 
     private static final String TAG = "BucketList App";
+
+    private static final String LOG_TAG = "Google Places Autocomplete";
+    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    private static final String OUT_JSON = "/json";
+    private static final String API_KEY = "AIzaSyAeb-71G4ZxW9cqvBn1ICiHiBDyQj71C4c";
 
     /* Client used to interact with Google APIs. */
     private GoogleApiClient mGoogleApiClient;
@@ -90,13 +111,10 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
 
         mapFragment.getMapAsync(this);
 
-        searchBox = (SearchView)findViewById(R.id.searchBox);
-
+        //***setOnQueryTextListener***
+        /*searchBox = (SearchView)findViewById(R.id.searchBox);
         searchBox.setBackgroundColor(Color.WHITE);
         searchBox.getBackground().setAlpha(205);
-
-
-        //***setOnQueryTextListener***
         searchBox.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
             @Override
@@ -118,7 +136,12 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
 
                 return false;
             }
-        });
+        });*/
+
+        AutoCompleteTextView autoCompView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        autoCompView.setAdapter(new GooglePlacesAutocompleteAdapter(this, R.layout.list_item));
+        autoCompView.setOnItemClickListener(this);
+
 
         mydb = new DBHelper(this);
         dbUserId = getIntent().getLongExtra("DB_USER_ID", -1);
@@ -156,6 +179,79 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
         });
 
      }
+
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        String str = (String) adapterView.getItemAtPosition(position);
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Test: Autocomplete: onitemclick", Toast.LENGTH_LONG).show();
+    }
+
+    public ArrayList<String> autocomplete(String input) {
+        ArrayList<String> resultList = null;
+
+        Toast.makeText(getApplicationContext(), "Test: Autocomplete: autocomplete", Toast.LENGTH_LONG).show();
+
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+
+            Toast.makeText(this, "Test: Autocomplete: StringBuilder", Toast.LENGTH_LONG).show();
+
+            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+            sb.append("?key=" + API_KEY);
+            sb.append("&components=country:gr");
+            sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+
+            URL url = new URL(sb.toString());
+
+            System.out.println("URL: "+url);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Load the results into a StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Toast.makeText(this, "Test: Autocomplete: Error processing Place", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "Error processing Places API URL", e);
+            return resultList;
+        } catch (IOException e) {
+            Toast.makeText(this, "Test: Autocomplete: Error connecting to Place", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "Error connecting to Places API", e);
+            return resultList;
+        }
+        finally {
+            if (conn != null) {
+                conn.disconnect();
+                Toast.makeText(this, "Test: Autocomplete:  conn.disconnect();", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        try {
+
+            Toast.makeText(this, "Test: Autocomplete: JSONObject", Toast.LENGTH_LONG).show();
+
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(jsonResults.toString());
+            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+            // Extract the Place descriptions from the results
+            resultList = new ArrayList<String>(predsJsonArray.length());
+            for (int i = 0; i < predsJsonArray.length(); i++) {
+                System.out.println(predsJsonArray.getJSONObject(i).getString("description"));
+                System.out.println("============================================================");
+                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Cant process JSON results", e);
+            Toast.makeText(this, "Test: Autocomplete: Cant process JSON results", Toast.LENGTH_LONG).show();
+        }
+
+        return resultList;
+    }
 
     @Override
     public void onMapReady(GoogleMap map) {
@@ -434,6 +530,54 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
             lastLocalKnownLocation = location;
             mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
             mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        }
+    }
+
+    //nested class object which implements the auto complete function
+    class GooglePlacesAutocompleteAdapter extends ArrayAdapter<String> implements Filterable {
+        private ArrayList<String> resultList;
+
+        public GooglePlacesAutocompleteAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+        }
+
+        @Override
+        public int getCount() {
+            return resultList.size();
+        }
+
+        @Override
+        public String getItem(int index) {
+            return resultList.get(index);
+        }
+
+        @Override
+        public Filter getFilter() {
+            Filter filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults filterResults = new FilterResults();
+                    if (constraint != null) {
+                        // Retrieve the autocomplete results.
+                        resultList = autocomplete(constraint.toString());
+
+                        // Assign the data to the FilterResults
+                        filterResults.values = resultList;
+                        filterResults.count = resultList.size();
+                    }
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    if (results != null && results.count > 0) {
+                        notifyDataSetChanged();
+                    } else {
+                        notifyDataSetInvalidated();
+                    }
+                }
+            };
+            return filter;
         }
     }
 }
